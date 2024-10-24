b93587d32b77977ebd353fbcf3e9c068f371adae
Jonnybvb76 <jonnybvb76@gmail.com>
2024-10-24 15:55:13 +0200

    Change: changed routing for API (for better understandig and api.zco.one domain)

--- api/server.js
+++ api/server.js
@@ -8,11 +8,11 @@ const port = process.env.API_PORT || 24402;
 
 app.use(express.json());
 
-app.post('/api/shorten', verifyToken, shortenUrl);
+app.post('/v1/shorten', verifyToken, shortenUrl);
 
-app.get('/api/count', verifyToken, getUrlCount);
+app.get('/v1/count', verifyToken, getUrlCount);
 
-app.get('/api/:shortUrl', getOriginalUrl);
+app.get('/v1/:shortUrl', getOriginalUrl);
 
 app.listen(port, () => {
     console.log(`API running on http://localhost:${port}`);
